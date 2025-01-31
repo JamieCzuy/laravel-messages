ad047e49e6a3afc98ce5ec1c04b20775183d7f87
JamieCzuy <jamie.czuy@gmail.com>
2025-01-31 10:51:02 -0500

    Update the curl tests

--- curl_tests/get_the_root.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-SCRIPT_NAME=$(basename "$0")
-OUTPUT_FILE="${SCRIPT_NAME}.json"
-
-./call_curl.sh
--- curl_tests/register_a_user.sh
+++ curl_tests/register_a_user.sh
@@ -1,29 +1,10 @@
 #!/usr/bin/env bash
 # set -x
 
+# ---- Handle Output File ---- #
 SCRIPT_NAME=$(basename "$0")
-
-
 export OUTPUT_FILE="${SCRIPT_NAME}.json"
-
-
-# export URL_PATH="/auth/register"
-# export URL_PATH="/register"
-
-# ---- Set the Post Data ---- #
-# export POST_DATA=${POST_DATA:-'{
-#   "email": "jamie.czuy+01_laravel_test@gmail.com",
-#   "password": "Test123",
-#   "first_name": "Jamie01",
-#   "last_name": "Tester"
-# }'}
-
-export POST_DATA=${POST_DATA:-'{"email": "jamie.czuy+01_laravel_test@gmail.com", "password": "Password123!", "first_name": "Jamie01", "last_name": "Tester"}'}
-# export POST_DATA=${POST_DATA:-'{"email": "jamie.czuy+02_laravel_test@gmail.com", "password": "Password123!", "first_name": "Jamie02", "last_name": "Tester"}'}
-# export POST_DATA=${POST_DATA:-'{"email": "jamie.czuy+03_laravel_test@gmail.com", "password": "Password123!", "first_name": "Jamie03", "last_name": "Tester"}'}
-# export POST_DATA=${POST_DATA:-'{"email": "jamie.czuy+04_laravel_test@gmail.com", "password": "Password123!", "first_name": "Jamie04", "last_name": "Tester"}'}
-
-
+rm -f ${OUTPUT_FILE}
 
 # ---- Build the URL ---- #
 URL_METHOD="POST"
@@ -40,12 +21,8 @@ echo "  Method: ${URL_METHOD}"
 echo "  URL: ${URL}"
 echo "  Output File: ${OUTPUT_FILE}"
 
-# ---- Handle Output File ---- #
-OUTPUT_FILE=${OUTPUT_FILE:-"output.json"}
-rm -f ${OUTPUT_FILE}
-
 # ---- Make the curl call ---- #
-NUM=04
+NUM=01
 
 curl \
   -H "Accept: application/json" \
@@ -58,9 +35,8 @@ curl \
   -X ${URL_METHOD} \
  ${URL}
 
-# ---- Show the results (assumes json) ---- #
+# ---- Show the results (assumes json or html) ---- #
 cat ${OUTPUT_FILE} | jq .
-
 
 cp ${OUTPUT_FILE} output.html
 open output.html

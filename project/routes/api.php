a9e3b434387ab78b18a5ccf17b99bfbee33afc28
JamieCzuy <jamie.czuy@gmail.com>
2025-01-31 02:13:04 -0500

    add new endpoint /api/register and move old to /auth/register

--- project/routes/api.php
+++ project/routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\Api\NewRegisteredUserController;
+
+Route::post('/register', [NewRegisteredUserController::class, 'store'])
+    ->middleware('guest')
+    ->name('register');
+
+require __DIR__.'/auth.php';
